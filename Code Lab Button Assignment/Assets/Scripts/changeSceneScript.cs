de3a06b06486b2fd15925fd432c8483621e84dd1
jjerion <56169080+jjerion@users.noreply.github.com>
2019-11-25 11:42:09 -0500

    upload assignment

---
+++ Code Lab Button Assignment/Assets/Scripts/changeSceneScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeSceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GotoLongBeach()
+    {
+        SceneManager.LoadScene("LongBeach"); //switch to long beach scene
+    }
+
+    public void GotoOlympia()
+    {
+        SceneManager.LoadScene("Olympia"); //switch to olympia scene
+    }
+}
